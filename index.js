0f6f792fda32d2062dd88e82ae604f9d4fadc31e
Ashwin Mukhija <achromatic.rainbow@gmail.com>
2015-08-06 23:29:04 +0530

    Basic tokenization logic

--- index.js
+++ index.js
@@ -33,8 +33,16 @@ const testsMapper = (test) => {
   const correctedMatchedKW = kwExtractor(kw => distanceMatcher(kw.keyword, test) <= THRESHOLD, keywordList);
   const directSynMatchedKW = kwExtractor(R.where({ syns: R.contains(test) }), keywordList);
   const correctedSynMatchedKW = kwExtractor(kw => R.length(R.filter(syn => distanceMatcher(syn, test) <= THRESHOLD, kw.syns)), keywordList);
-  return directMatchedKW || correctedMatchedKW || directSynMatchedKW || correctedSynMatchedKW;
+  return directMatchedKW || correctedMatchedKW || directSynMatchedKW || correctedSynMatchedKW || test;
 };
 
 const computedResults = R.map(testsMapper, tests);
 console.log(R.equals(expectedResults, computedResults));
+
+const testText = '1 vaccuum, several chests of drawers, 2 sodas';
+const tokenized = testText.split(/\s+/);
+console.log(tokenized);
+const punctuatedTokens = R.flatten(R.map((token) => token.split(/\b/), tokenized));
+console.log(punctuatedTokens);
+const correctedTokens = R.map(testsMapper, punctuatedTokens);
+console.log(correctedTokens.join(' '));
